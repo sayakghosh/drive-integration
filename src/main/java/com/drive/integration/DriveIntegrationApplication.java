e89200174cfb8bdab7254a89728e2d93c6e625e9
Sayak Ghosh <sayak96@gmail.com>
2022-10-08 11:26:52 +0530

    Initial commit to setup the project

---
+++ src/main/java/com/drive/integration/DriveIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.drive.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drive/integration/DriveIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drive.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
