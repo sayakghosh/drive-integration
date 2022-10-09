c4a6681bfa7d21a93ffbb59b2a3c15d53d7e4c5e
Sayak Ghosh <sayak96@gmail.com>
2022-10-09 18:11:41 +0530

    Updating constants to remove duplicates

--- src/main/java/com/drive/integration/common/Constant.java
+++ src/main/java/com/drive/integration/common/Constant.java
@@ -7,6 +7,5 @@ public class Constant {
 	
     public static final long DEFAULT_FIXED_DELAY = 20000;
     public static final String DEFAULT_FILE_LOCATION = "drive_integration.txt";
-    //public static final String DEFAULT_FILE_LOCATION = "/Users/sayakghosh/Desktop/Sayak/test_files/drive_integration.txt";
     
 }
